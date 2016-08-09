800ac52558cefb0765e671e5acbd6fc1bde3b8cb
Thad House <thadhouse1@gmail.com>
2016-08-08 23:05:23 -0700

    Reenables NtCore builds

--- netcore.sh
+++ netcore.sh
@@ -30,7 +30,7 @@ function Test {
   
    dotnet test ./test/NetworkTables.Test $configuration -f netcoreapp1.0
   
-  if [ "skipNtCore" = false ] ; then
+  if [ "$skipNtCore" = false ] ; then
      dotnet test ./test/NetworkTables.Core.Test $configuration -f netcoreapp1.0
   fi
 
@@ -38,7 +38,7 @@ function Test {
 
   mono test/NetworkTables.Test/$libLoc/net451/*/dotnet-test-nunit.exe test/NetworkTables.Test/$libLoc/net451/*/NetworkTables.Test.dll 
   
-  if [ "skipNtCore" = false ] ; then
+  if [ "$skipNtCore" = false ] ; then
     dotnet build ./test/NetworkTables.Core.Test $configuration -f net451
   
     mono test/NetworkTables.Core.Test/$libLoc/net451/*/dotnet-test-nunit.exe test/NetworkTables.Core.Test/$libLoc/net451/*/NetworkTables.Core.Test.dll 
